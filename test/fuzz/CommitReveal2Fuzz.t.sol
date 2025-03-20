2445ea0e64fdc77048e61a10a828f3b44896b05c
Euisin Gee <64007655+usgeeus@users.noreply.github.com>
2025-03-20 11:16:52 +0900

    feat: Add inline assembly vs. Solidity Merkle Root creation fuzz tests
    - Introduces CommitReveal2Fuzz contract to compare gas usage between inline assembly and Solidity Merkle root creation
    - Adds CreateMerkleRootSolidity and CreateMerkleRootInlineAssembly contracts for side-by-side testing

---
+++ test/fuzz/CommitReveal2Fuzz.t.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {CreateMerkleRootSolidity, CreateMerkleRootInlineAssembly} from "./../../src/test/CommitReveal2Test.sol";
+
+import {NetworkHelperConfig} from "./../../script/NetworkHelperConfig.s.sol";
+import {Test, console2} from "forge-std/Test.sol";
+
+contract CommitReveal2Fuzz is Test {
+    CreateMerkleRootSolidity createMerkleRootSolidity;
+    CreateMerkleRootInlineAssembly createMerkleRootInlineAssembly;
+
+    function setUp() public {
+        NetworkHelperConfig networkHelperConfig = new NetworkHelperConfig();
+        NetworkHelperConfig.NetworkConfig memory activeNetworkConfig = networkHelperConfig.getActiveNetworkConfig();
+        vm.deal(address(this), 10000 ether);
+
+        createMerkleRootSolidity = new CreateMerkleRootSolidity{value: activeNetworkConfig.activationThreshold}(
+            activeNetworkConfig.activationThreshold,
+            activeNetworkConfig.flatFee,
+            activeNetworkConfig.maxActivatedOperators,
+            activeNetworkConfig.name,
+            activeNetworkConfig.version,
+            activeNetworkConfig.offChainSubmissionPeriod,
+            activeNetworkConfig.requestOrSubmitOrFailDecisionPeriod,
+            activeNetworkConfig.onChainSubmissionPeriod,
+            activeNetworkConfig.offChainSubmissionPeriodPerOperator,
+            activeNetworkConfig.onChainSubmissionPeriodPerOperator
+        );
+
+        createMerkleRootInlineAssembly = new CreateMerkleRootInlineAssembly{
+            value: activeNetworkConfig.activationThreshold
+        }(
+            activeNetworkConfig.activationThreshold,
+            activeNetworkConfig.flatFee,
+            activeNetworkConfig.maxActivatedOperators,
+            activeNetworkConfig.name,
+            activeNetworkConfig.version,
+            activeNetworkConfig.offChainSubmissionPeriod,
+            activeNetworkConfig.requestOrSubmitOrFailDecisionPeriod,
+            activeNetworkConfig.onChainSubmissionPeriod,
+            activeNetworkConfig.offChainSubmissionPeriodPerOperator,
+            activeNetworkConfig.onChainSubmissionPeriodPerOperator
+        );
+    }
+
+    function testFuzz_CreateMerkleRoot(bytes32[] memory leaves) public view {
+        vm.assume(leaves.length > 1);
+        bytes32 rootSolidity = createMerkleRootSolidity.createMR(leaves);
+        uint256 gasUsedSolidity = vm.lastCallGas().gasTotalUsed;
+        bytes32 rootInlineAssembly = createMerkleRootInlineAssembly.createMR(leaves);
+        uint256 gasUsedInlineAssembly = vm.lastCallGas().gasTotalUsed;
+        assertGt(gasUsedSolidity, gasUsedInlineAssembly);
+        assertEq(rootSolidity, rootInlineAssembly);
+    }
+}
