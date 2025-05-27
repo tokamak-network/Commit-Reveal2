88780af0540c45c7931b0749ff6553a4bdd08810
Euisin Gee <64007655+usgeeus@users.noreply.github.com>
2025-05-27 10:07:07 +0900

    feat: enhance request information storage in ConsumerExample contract
    This commit adds a new field for the requester address in the RequestInfo struct and renames the mapping for request information to improve clarity. The requestRandomNumber function is updated to store the requester address along with the request block number and fee, enhancing the tracking of random number requests.

--- src/ConsumerExample.sol
+++ src/ConsumerExample.sol
@@ -11,6 +11,7 @@ contract ConsumerExample is ConsumerBase {
 
     struct RequestInfo {
         uint256 requestBlockNumber;
+        address requester;
         uint256 requestFee;
         uint256 fulfillBlockNumber;
     }
@@ -19,7 +20,7 @@ contract ConsumerExample is ConsumerBase {
 
     mapping(address requester => uint256[] requestIds) public s_requesterRequestIds;
     mapping(uint256 requestId => RequestStatus) public s_requests; /* requestId --> requestStatus */
-    mapping(uint256 requestId => RequestInfo) public s_blockNumbers;
+    mapping(uint256 requestId => RequestInfo) public s_requestInfos;
 
     // * for testing
     uint256 public lastRequestId;
@@ -32,18 +33,19 @@ contract ConsumerExample is ConsumerBase {
     function requestRandomNumber() external payable {
         (uint256 requestId, uint256 requestFee) = _requestRandomNumber(CALLBACK_GAS_LIMIT);
         s_requesterRequestIds[msg.sender].push(requestId);
-        s_blockNumbers[requestId].requestBlockNumber = block.number;
-        s_blockNumbers[requestId].requestFee = requestFee;
+        s_requestInfos[requestId].requestBlockNumber = block.number;
+        s_requestInfos[requestId].requestFee = requestFee;
+        s_requestInfos[requestId].requester = msg.sender;
     }
 
     function fulfillRandomRandomNumber(uint256 requestId, uint256 randomNumber) internal override {
         s_requests[requestId] = RequestStatus(true, randomNumber);
-        s_blockNumbers[requestId].fulfillBlockNumber = block.number;
+        s_requestInfos[requestId].fulfillBlockNumber = block.number;
         lastRequestId = requestId;
     }
 
     function getCommitReveal2Address() external view returns (address) {
-        return address(i_commitreveal2);
+        return address(s_commitreveal2);
     }
 
     function getYourRequests()
